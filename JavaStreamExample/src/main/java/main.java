49931f1536aa7964ee98cf6ba277669851268c38
JonatanHjelm95 <cph-jh409@cphbusiness.dk>
2020-04-14 09:38:58 +0200

    init

---
+++ JavaStreamExample/src/main/java/main.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.*;
+import java.util.Random; /**
+         *
+         * @author jonab
+         */
+
+public class main {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        List<Integer> integerList = new ArrayList();
+        for (int i = 0; i < 1000; i++) {
+            integerList.add(r.nextInt(1000));
+        }
+        long skip = 300;
+        List<Integer> sortedList = integerList.stream().skip(skip).sorted().collect(Collectors.toList());
+        for (int i = 0; i < sortedList.size(); i++) {
+            System.out.println(sortedList.get(i));
+        }
+
+    }
+}
